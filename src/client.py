6816357289e5002c4b226818815a1b94ef69dc8e
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2022-10-29 00:52:12 +0800

    Packs into class

---
+++ src/client.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+
+
+import socket
+import tqdm
+import os
+import time
+import pickle
+import signal
+
+"""
+client:
+// startConnect()
+// getHeader()
+// recvFile()
+// stopConnect()
+"""
+
+class Client():
+    def __init__(self, host, port, save_folder, file_location='./', file_name='', file_size=0, chunk_size=4096, connection=False):
+        self.host = host
+        self.port = port
+        
+        self.save_folder = save_folder
+        self.file_location = file_location
+        self.file_name = file_name
+        self.file_size = file_size
+        self.chunk_size = chunk_size
+        self.connection = connection
+
+    def start(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print(f'start connecting {self.host}:{self.port}')
+
+        retry = 0
+        while not self.connection:
+            try:
+                self.client.connect((self.host, self.port))
+                self.connection = True
+                print(f'Connect to server successfully {self.client.getpeername()[0]}:{self.client.getpeername()[1]}')
+                self.connection = True
+                
+            except:
+                if retry > 10: 
+                    self.connection = False
+                    return print('Fail to connect')
+                retry += 1
+                print('Fail to connect, try again')
+                pass
+
+    def getHeader(self):
+        retry = 0
+        self.client.settimeout(5)
+        while self.connection:
+            try:
+                receive = self.client.recv(self.chunk_size)
+                break
+            except:
+                if retry > 4: 
+                    self.connection = False
+                    return print('Fail to getHeader')
+                retry += 1
+                print('Fail to getHeader, try again')
+                pass
+
+        self.client.settimeout(None)
+        header = pickle.loads(receive)
+        print(header)
+
+        self.file_name = header['file_name']
+        self.file_size = header['file_size']
+
+        if not self.receivedResponse():
+            self.connection = False
+            return print('Fail to response')
+        return
+    
+    def getFile(self):
+        self.file_location = f'{self.save_folder}/received_{getTime()}_{self.file_name}'
+
+        with open(self.file_location, 'wb') as f:
+            progress = tqdm.tqdm(range(int(self.file_size)), f'receive {self.file_name}', unit='K', unit_divisor=1024, unit_scale=True)
+
+            self.client.settimeout(1)
+            while True:
+                try:
+                    bytes_read = self.client.recv(self.chunk_size) # read data from server
+                except:
+                    self.connection = False
+                    return print('\nFail to get file')
+
+                f.write(bytes_read)
+                progress.update(len(bytes_read))
+
+                if not bytes_read:
+                    print("\n--all file received")
+                    return self.stop()
+
+
+    def receivedResponse(self):
+        response = 'RECEIVED'
+        package = pickle.dumps(response)
+        try:
+            self.client.sendall(package)
+        except:
+            print('Fail to send response')
+            self.connection = False
+            return False
+        return True
+
+    def stop(self):
+        self.connection = False
+        self.client.close()
+        return print("close connection")
+
+
+
+
+
+
+
+def getTime():
+    localtime = time.localtime()
+    result = time.strftime("%I-%M-%S", localtime)
+    return str(result)
+
+
+
+
+
+
+
+
+"""
+if __name__ == '__main__':
+    server_host = '127.0.0.1'#'192.168.31.146'
+    server_port = 7777
+    save_folder = './save'
+
+    client = Client(server_host, server_port, save_folder)
+    client.start()
+    client.getHeader()
+    client.getFile()
+"""
---
+++ src/server.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+
+
+import socket
+import time
+import tqdm
+import os
+import pickle
+
+"""
+server:
+packageFile()
+// startServer()
+// sendHeader()
+// sendFile()
+// stopServer()
+"""
+
+class Server():
+    def __init__(self, host, port, file_name, file_location, max_listening=1, chunk_size=4096, connection=False):
+        self.host = host
+        self.port = port
+
+        self.file_name = file_name
+        self.file_location = file_location
+
+        self.max_listening = max_listening
+        self.chunk_size = chunk_size
+        self.connection = connection
+
+
+    def start(self):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind((self.host, self.port))
+        self.server.listen(self.max_listening)
+        print(f'server start listening {self.server.getsockname()[0]}:{self.server.getsockname()[1]}')
+
+        self.client, client_address = self.server.accept()
+        print(f'client connection success {self.client.getpeername()[0]}:{self.client.getpeername()[1]}')
+        self.connection = True
+        return
+
+    def sendHeader(self):
+        if not self.connection:
+            return print('Client not connect.')
+
+        self.file_size = os.path.getsize(self.file_location) # get file size
+
+        header = {
+            'file_name' : self.file_name,
+            'file_size' : self.file_size # bytes
+        }
+
+        print(header)
+        package = pickle.dumps(header)
+        try:
+            self.client.sendall(package)
+        except:
+            self.connection = False
+            return print("Fail to send header")
+
+        print('send header successfully')
+
+        if not self.waitReceive():
+            self.connection = False
+            return print('Client disconnect')
+        return
+
+    def sendFile(self):
+        if not self.connection:
+            return print('Client not connect.')
+
+        with open(self.file_location, 'rb') as f:
+            progress = tqdm.tqdm(range(int(self.file_size)), f'send{self.file_size}', unit='K', unit_divisor=1024)
+
+            for _ in progress:
+                bytes_read = f.read(self.chunk_size) # read file
+
+                if not bytes_read:
+                    print("\n--all send successfully")
+                    return self.stop()
+
+                try:
+                    self.client.sendall(bytes_read)
+                    progress.update(len(bytes_read))
+                except :
+                    self.connection = False
+                    return print("\nclient close connection")
+                
+
+    def waitReceive(self):
+        retry = 0
+        while self.connection:
+            try:
+                self.client.settimeout(5)
+                data = self.client.recv(self.chunk_size)
+                if pickle.loads(data) == 'RECEIVED':
+                    print('Client received data')
+                    self.client.settimeout(None)
+                    return True
+            except:
+                print('--retry')
+                if retry > 4: 
+                    self.client.settimeout(None)
+                    self.connection = False
+                    return False
+                retry += 1
+
+    def stop(self):
+        self.connection = False
+        self.client.close()
+        return print("close connection")
+
+    def status(self):
+        if not self.connection:
+            return print('Client not connect.')
+
+        print(f'server working on {self.server.getsockname()[0]}:{self.server.getsockname()[1]}')
+        
+
+"""
+if __name__ == '__main__':
+    server_host = '127.0.0.1'#'192.168.31.146'
+    server_port = 7777
+    server_filename = '01.txt'
+    server_filelocation = './tmp/test.7z'
+
+    server = Server(server_host, server_port, server_filename, server_filelocation)
+    
+    while True:
+        server.start()
+        #server.waitReceive()
+        print('------time.sleep(5)-----')
+        #time.sleep(8)
+        server.status()
+        server.sendHeader()
+        server.sendFile()
+        server.stop()
+"""
