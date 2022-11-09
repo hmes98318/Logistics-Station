c4c6cadb3ba15e3da7568a8e18a40bac9a532c7a
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2022-11-10 02:11:39 +0800

    Update test files

--- testClient.py
+++ testClient.py
@@ -1,13 +1,21 @@
 from src.client import Client
+import time
 
 
 if __name__ == '__main__':
     server_host = '192.168.31.146'#'127.0.0.1'#
     server_port = 7777
-    save_folder = './save'
+    save_folder = 'C:/Users/usr/Desktop/p2py/save'#'C:\\Users\\usr\\Desktop\\p2py' #'./save'
 
-    client = Client(server_host, server_port, save_folder)
+    client = Client()
+    client.setHost(server_host, server_port)
+    client.setFolder(save_folder)
+
     client.start()
-    client.getHeader()
-    client.getFile()
+    client.askHeader()
+    time.sleep(1)
+    print(f'connection: {client.showConnection()}')
+    time.sleep(1)
+    client.askFile()
     client.stop()
+    print(f'connection: {client.showConnection()}')
--- testServer.py
+++ testServer.py
@@ -5,15 +5,12 @@ import time
 if __name__ == '__main__':
     server_host = '192.168.31.146'#'0.0.0.0'#'127.0.0.1'#
     server_port = 7777
-    server_filename = 'test.7z'
-    server_filelocation = './tmp/test.7z'
+    server_filelocation = './tmp/01.txt'
 
-    server = Server(server_host, server_port, server_filename, server_filelocation)
     
-    while True:
-        server.start()
-        time.sleep(8)
-        server.status()
-        server.sendHeader()
-        server.sendFile()
-        server.stop()
+    server = Server()
+    server.setHost(server_host, server_port)
+    server.setFile(server_filelocation)
+    server.init()
+    server.startListening()
+    #server.stop()
