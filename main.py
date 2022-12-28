b12752a6d4dee9629eefa8e672965699baaf3be2
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2022-12-29 03:14:11 +0800

    Delete old code

--- main.py
+++ main.py
@@ -2,12 +2,7 @@
 
 import sys
 
-from src.gui.controller import *
 
 
 if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    LoginWindow = LoginWindow_controller()
-
-    LoginWindow.show()
-    sys.exit(app.exec_())
+    
--- src/data/UserData.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import sys
-
-userData = {
-        'user': 'password',
-        'test': 'test',
-        't': 't'
-        }
