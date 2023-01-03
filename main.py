7e9fbac022b0988aa6485292999d899473ee279d
落櫻雪月 <testuseroozx@gmail.com>
2023-01-04 02:07:01 +0800

    UpData main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import sys
-
+from gui.controller import *
 
 
 if __name__ == '__main__':
-    
+    app = QtWidgets.QApplication(sys.argv)
+    LoginWindow = LoginWindow_controller()
+
+    LoginWindow.show()
+    sys.exit(app.exec_())
