2cceb98190ae501d40d67f37112d050f7396b4d5
落櫻雪月 <testuseroozx@gmail.com>
2023-01-08 00:49:10 +0800

    Change UserPage - hyperlink

--- src/gui/controller.py
+++ src/gui/controller.py
@@ -197,14 +197,14 @@ class MainWindow_controller(QtWidgets.QWidget):
         self.ui.progressBar_RecevieFile.setValue(0) # 進度條 0 
         self.ui.progressBar_SendFile.setValue(0)
         
-        # --------------測試區域----------------------------------------
+        # --------------標籤超連結----------------------------------------
         
-        self.ui.label_sakura0711.setText('  ‧<a href="https://github.com/sakura0711?tab=repositories">sakura0711</a>')
-        self.ui.label_sakura0711.setOpenExternalLinks(True)  # 使其成為超連結
-        self.ui.label_sakura0711.setTextInteractionFlags(Qt.TextBrowserInteraction)  # 雙擊可以選擇
-        self.ui.label_hmes98318.setText('  ‧<a href="https://github.com/hmes98318?tab=repositories">hmes98318</a>')
+        self.ui.label_hmes98318.setText('  ‧<a href="https://github.com/hmes98318">hmes98318</a>')
         self.ui.label_hmes98318.setOpenExternalLinks(True)  # 使其成為超連結
         self.ui.label_hmes98318.setTextInteractionFlags(Qt.TextBrowserInteraction)  # 雙擊可以選擇
+        self.ui.label_sakura0711.setText('  ‧<a href="https://github.com/sakura0711">sakura0711</a>')
+        self.ui.label_sakura0711.setOpenExternalLinks(True)  # 使其成為超連結
+        self.ui.label_sakura0711.setTextInteractionFlags(Qt.TextBrowserInteraction)  # 雙擊可以選擇
 
         # ------------------------------------------------------------
 
